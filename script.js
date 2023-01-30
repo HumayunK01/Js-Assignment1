923838baba9fe392a6d7cf0d975e85b245b1b0bc
Humayun Khan <humayunk.pvt@gmail.com>
2023-01-30 21:42:01 +0530

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+function dark() {
+  document.body.style.backgroundColor = "#121212";
+  document.body.style.color = "#ffffff";
+}
+
+function light() {
+  document.body.style.backgroundColor = "#ffffff";
+  document.body.style.color = "#121212";
+}
+
+function semi_dark() {
+  document.body.style.backgroundColor = "#414A4C";
+  document.body.style.color = "#ffffff";
+}
